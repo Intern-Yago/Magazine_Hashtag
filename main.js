50116784b0d6b6cb5c58a2089a1957305100c68a
Intern-Yago <yagovictorbotafogo@gmail.com>
2023-08-30 23:09:47 -0300

    second class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
 const catalogo = [
     {
       id: 1,
@@ -67,12 +69,12 @@ const catalogo = [
 
 for(const produtoCatalogo of catalogo){
     const cartaoProduto = `
-        <div id="card-produto-${produtoCatalogo.id}">
+        <div class="boder-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalogo.id}">
             <img src="./assets/img/${produtoCatalogo.imagem}" 
                 alt="Produto 1"
                 style="height: 300px;"
             />
-            <p>${produtoCatalogo.marca}</p>
+            <p class="marca">${produtoCatalogo.marca}</p>
             <p>${produtoCatalogo.nome}</p>
             <p>$${produtoCatalogo.preco}</p>
             <button>Adiconar</button>
@@ -80,3 +82,5 @@ for(const produtoCatalogo of catalogo){
     `
     document.getElementById("container-produto").innerHTML += cartaoProduto
 }
+
+inicializarCarrinho()
